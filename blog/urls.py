84d137e6dbbdd66f7a10993e2bbee3af80b0aaf2
Ryan <ryanjaldred@gmail.com>
2015-11-06 15:51:17 -0800

    added comment features

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,24 @@ from . import views
 
 
 urlpatterns = [
+	# view the blog
 	url(r'^$', views.post_list, name='post_list'),
+	# view the full post
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+	# write a new post
 	url(r'^post/new/$', views.post_new, name='post_new'),
+	# edit a post
 	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+	# view page of drafted posts
 	url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
+	# publish the drafted posts
 	url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
+	# remove post
 	url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
+	# add post comment
+	url(r'^post/(?P<pk>[0-9]+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+	# to approve a comment
+	url(r'^comment/(?P<pk>[0-9]+)/approve/$', views.comment_approve, name='comment_approve'),
+	# to remove a comment
+	url(r'^comment/(?P<pk>[0-9]+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
